b991c20e21e9ea468c0a40d0f0637b16f8731439
lunardog <leszek@rybicki.cc>
2016-07-15 13:57:59 +0900

    add config sample file

---
+++ src/config-sample.js
@@ -0,0 +1,8 @@
+export default {
+  firebase: {
+    apiKey: "QWERTY1234",
+    authDomain: "your-app-domain.firebaseapp.com",
+    databaseURL: "https://your-app-domain.firebaseio.com",
+    storageBucket: "your-app-domain.appspot.com",
+  }
+}
