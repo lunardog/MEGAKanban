fd53d6c99f1979a829b2e8fa06d406dfec7c7919
Seito Taka <amanoese@gmail.com>
2016-08-02 19:20:06 +0900

    landing page background created

---
+++ src/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="landing"></div>
+</template>
+
+<style>
+#landing {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-image: linear-gradient(to bottom, #242424, #7b7b7b);
+}
+</style>
+
